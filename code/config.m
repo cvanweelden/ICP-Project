45d4d87838c34d7e88f47b7400c8d127a3b95fe9
Thomas van den Berg <paranoid.ndroid@gmail.com>
2011-12-02 17:35:54 +0100

    edit config

--- code/config.m
+++ code/config.m
@@ -1,3 +1,3 @@
 % Set the relative path to your compiled KinectMatlab.
 KINECT_MATLAB_PATH = '../../kinectmatlab';
-DATA_FILES_PATH = '~/Dropbox/Studie/ICP-Project/data';
+DATA_PATH = '~/Dropbox/Studie/ICP-Project/data';
