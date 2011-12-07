4c6b72bce5b6756cd6eade7f299753d6a0e0d7f6
Carsten van Weelden <cvanweelden@gmail.com>
2011-12-07 17:17:14 +0100

    Removed config.m from tracking, but hopefully not from your file tree ;)

--- code/config.m
+++
@@ -1,5 +0,0 @@
-% Set the relative path to your compiled KinectMatlab.
-KINECT_MATLAB_PATH = '../../kinectmatlab/Mex';
-DATA_PATH = '~/Dropbox/Studie/ICP-Project/data';
-
-addpath(KINECT_MATLAB_PATH);
