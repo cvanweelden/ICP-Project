806d579c252a65ceb781ebfbf2fd2eb22185ae1d
Carsten van Weelden <cvanweelden@gmail.com>
2011-12-08 12:28:17 +0100

    Implemented registration vector code, sec. III.C

---
+++ code/get_transformation.m
@@ -0,0 +1,24 @@
+function [ q_r q_t ] = get_transformation( P, X )
+%GET_TRANSFORMATION compute registration vector of data P to model X
+%   P and X are 3xN matrixes for N points in 3 dimensions, i.e. each column
+%   corresponds to a single point. See Besl & McKay (1992) section III.C.
+
+mu_p = sum(P,2)/size(P,2);
+mu_x = sum(X,2)/size(X,2);
+
+Sigma = (P*X'-mu_p*mu_x')/size(P,2);
+
+A = (Sigma - Sigma');
+Delta = [A(2,3) A(3,1) A(1,2)]';
+
+tr = trace(Sigma);
+Q = [tr Delta'; Delta (Sigma + Sigma' - tr*eye(3))];
+
+[V,D] = eig(Q);
+[maxValue,index] = max(diag(D));
+q_r = V(:,index);
+
+q_t = mu_x - quad2rot(q_r) * mu_p;
+
+end
+
