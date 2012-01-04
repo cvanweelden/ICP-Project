be478a44ee67b010c84342f5cdf6bc301b9bf2bc
Thomas van den Berg <paranoid.ndroid@gmail.com>
2012-01-04 23:52:59 +0100

    add point cloud rendering

--- code/im2pixels.m
+++ code/im2pixels.m
@@ -3,9 +3,9 @@ function [ pixels ] = im2pixels( im , mask)
     if (nargin > 1)
         mask3 = cat(3,mask,mask,mask);
         pixels = im(mask3);
-        pixels = reshape(pixels, sum(sum(mask)), 3);
+        pixels = reshape(pixels, sum(sum(mask)), 3)';
     else
-        pixels = reshape(im, size(im,1)*size(im,2), 3);
+        pixels = reshape(im, size(im,1)*size(im,2), 3)';
     end
 end
 
--- code/test_icp.m
+++ code/test_icp.m
@@ -1,11 +1,11 @@
-bla = load([DATA_PATH 'DandI.mat']);
+bla = load([DATA_PATH '10framesRGBD.mat']);
 
 D = bla.D;
 
 figure;
 
 D1 = D{1}(1:25:end, 1:25:end, :);
-D1 = im2pixels(D1)';
+D1 = im2pixels(D1);
 
 D1(:,D1(1,:)==0 & D1(2,:)==0 & D1(3,:)==0) = [];
 hold on;
@@ -14,7 +14,7 @@ scatter3(D1(1,:), D1(2,:), D1(3,:), 1, 'b')
 for i = 2:10
 
     D2 = D{i}(1:25:end, 1:25:end, :);
-    D2 = im2pixels(D2)';
+    D2 = im2pixels(D2);
     D2(:,D2(1,:)==0 & D2(2,:)==0 & D2(3,:)==0) = [];
 
     [D2_alligned mse] = icp(D2, D1, 0.5);
