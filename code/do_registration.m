79108340ca600be77e394675eb581d111bdc243e
Thomas van den Berg <paranoid.ndroid@gmail.com>
2012-01-23 17:36:49 +0100

    add params to do_registration
    - rename Q to A to prevent confusion with q,t

--- code/do_registration.m
+++ code/do_registration.m
@@ -1,40 +1,48 @@
-function [results] = do_registration(input_dir, output_dir, icp_params)
+function [results] = do_registration(input_dir, output_dir, varargin)
 
-MAX_CLOUD_SIZE = 1e4;
 
-%Default parameters for the ICP method.
-if (nargin < 3)
-    icp_params.num_iterations = 50;
-    icp_params.mse_threshold = 0.01;
-    icp_params.debug = 0;
-end
+% Parse input arguments
+p = inputParser;
+p.addParamValue('ModelMode',     'global', @(x)strcmpi(x,'global') || strcmpi(x,'previous'));
+p.addParamValue('MaxCloudSize',  1e4, @(x)isnumeric(x));
+p.addParamValue('MaxIterations', 50, @(x)isnumeric(x));
+p.addParamValue('MSEThreshold',  1e-5, @(x)isnumeric(x));
+p.parse(varargin{:});
+
+cloudsize = p.Results.MaxCloudSize;
+maxiter = p.Results.MaxIterations;
+msethresh = p.Results.MSEThreshold;
 
 mkdir(output_dir);
-mkdir([output_dir filesep 'frames']);
+mkdir(fullfile(output_dir,'frames'));
 
-pci = PointCloudIterator(input_dir);
+pcs = PointCloudSet(input_dir);
 
-results.avg_mse = 0.0;
-results.mse_profile = cell(pci.num_frames-1,1);
-results.num_iter = zeros(pci.num_frames-1,1);
-results.transformations = cell(pci.num_frames-1,1);
+% results.avg_mse = 0.0;
+% results.mse_profile = cell(pci.num_frames-1,1);
+% results.num_iter = zeros(pci.num_frames-1,1);
+% results.transformations = cell(pci.num_frames-1,1);
 
 %Initialize the registration with the first depth frame.
 pci.next();
 writeply(pci.pointcloud, pci.rgb_values, [output_dir filesep 'frames' filesep 'frame1.ply']);
-model = subsample_pointcloud(pci.pointcloud, MAX_CLOUD_SIZE);
-Q_prev = eye(4);
+
+model = pcs{1};
+model.write(fullfile(output_dir,'frames','frame1.ply'));
+model.subsample(cloudsize);
+
+A_prev = eye(4);
 
 for i = 2:pci.num_frames
-    fprintf('Alligning frame %i\n', i);
+    fprintf('Aligning frame %i\n', i);
     
-    pci.next();
-    new_frame = subsample_pointcloud(pci.pointcloud, MAX_CLOUD_SIZE);
-    new_frame = apply_transform(new_frame, Q_prev);
+    new_frame = pcs{i};
+    new_frame.subsample(cloudsize)
+    new_frame.apply_matrix(A_prev);
     
     %Call the ICP method and save the results.
     [new_frame_alligned dQ mse mse_profile num_iter] = icp(new_frame, model, icp_params);
-    Q_prev = dQ * Q_prev;
+    A_prev = dA * A_prev;
     results.avg_mse = results.avg_mse + mse;
     results.mse_profile{i-1} = mse_profile;
     results.num_iter(i-1) = num_iter;
