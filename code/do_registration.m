e277b55773990fd227d61ae52a3ad544dee51e55
Thomas van den Berg <paranoid.ndroid@gmail.com>
2012-01-30 22:42:58 +0100

    simplify: less code, less bugs
    Belangrijkste: niet meer de hele frame transformeren/uitschrijven maar
    alleen de ge-subsampelde, dit schrijft sneller en geeft kleinere
    bestanden zodat we de resultaten makkelijk kunnen delen, je kan nog
    steeds wel zien of er iets mis gaat in de registration, en precieze
    performance moeten we toch uit de paths halen.

--- code/do_registration.m
+++ code/do_registration.m
@@ -25,13 +25,10 @@ pcs = PointCloudSet(input_dir);
 
 %Write the first frame with the initial pose.
 qt = [p.Results.StartOrientation p.Results.StartPosition];
-frame = pcs{1};
-frame.apply_qt(qt); 
-frame.write(fullfile(output_dir,'frames','frame1.ply'));
-
-%Initialize the registration with the first depth frame.
-model = frame.copy();
+model = pcs{1};
 model.subsample(cloudsize);
+model.apply_qt(qt); 
+model.write(fullfile(output_dir,'frames','frame1.ply'));
 
 results.avg_mse = 0.0;
 results.mse_profile{1} = [];
@@ -44,32 +41,25 @@ for i = 2:frameskip:min(pcs.num_frames, maxframes)
     fprintf('Aligning frame %i\n', i);
     
     % Compute normals for the model (TODO: here?)
-    model.computenormals();
     
     % Load up a new frame
     frame = pcs{i};
-    new_frame = frame.copy();
-    new_frame.subsample(cloudsize);
-    new_frame.computenormals();
-    new_frame.apply_qt(qt);
+    frame.subsample(cloudsize);
+    frame.apply_qt(qt);
     
     % Call the ICP method and save the results.
-    [dqt mse_profile] = gicp(new_frame, model, gicpargs{:});
+    [dqt mse_profile] = gicp(frame, model, gicpargs{:});
     qt = rigid_multiply(dqt, qt);
     results.avg_mse = results.avg_mse + mse_profile(end);
     results.mse_profile{i} = mse_profile;
     results.num_iter(i) = numel(mse_profile);
     results.transformations{i} = dqt;
     results.pose{i} = qt;
-    results.timestamp{i} = new_frame.timestamp;
+    results.timestamp{i} = frame.timestamp;
     
     % Write the new frame
-    frame.apply_qt(qt);
+    frame.apply_qt(dqt);
     frame.write(fullfile(output_dir, 'frames', sprintf('frame%d.ply',i)));
-    % TODO: Below is a bit awkward, this used to be 'new_frame_aligned',
-    % but there is a bit of a discrepancy between pointCloud classes
-    % and XYZ matrices in gicp. 
-    frame.subsample(cloudsize);
     
     % Update the model
     if strcmpi(modelmode, 'global')
--- code/experiments.m
+++ code/experiments.m
@@ -1,20 +1,21 @@
 function experiments()
 
+    global DATA_PATH DROPBOX_PATH
+
     function results = do_one(method, frameskip, modelmode)
         fn = [method '_' num2str(frameskip) '_' modelmode];
-        results = do_registration(dataset, fullfile(output_dir, fn), cs{:}, posor{:},...
+        results = do_registration(dataset, fullfile(output_dir, fn), cs{:},...% posor{:},...
                     'MaxFrames', 5, ...
                     'ModelMode', modelmode, ...
-                    'GICPArgs', {'Method', method, 'MaxIterations', 15},...
+                    'GICPArgs', {'Method', method, 'MaxIterations', 100},...
                     'FrameSkip', frameskip);
-        write_trajectory(results, fullfile(output_dir, [fn 'traj.txt']))
     end
 
 
-    dataset = '../../data/rgbd_dataset_freiburg1_xyz';
-    output_dir = '../output';
+    dataset = fullfile(DATA_PATH, 'rgbd_dataset_freiburg1_xyz');
+    output_dir = fullfile(DROPBOX_PATH, 'results');
     cs = {'MaxCloudSize', 1e4};
     posor = {'StartPosition', [0.1170 -1.1503 1.4005], 'StartOrientation', [-0.5709 0.6523 -0.3566 0.3486]};
 
-    do_one('gicp', 1, 'global');
+    do_one('icp', 1, 'global');
 end
