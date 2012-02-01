1b08f23ccbe14414e8b0a0469ea616e788b9a8e8
Carsten van Weelden <cvanweelden@gmail.com>
2012-02-01 12:13:30 +0100

    Got registration from ply files using ICP working in registration.cpp

--- cpp/registration.cpp
+++ cpp/registration.cpp
@@ -7,8 +7,14 @@
 
 #include <pcl/io/pcd_io.h>
 #include <pcl/io/ply_io.h>
+
 #include <pcl/point_types.h>
+
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+
+#include <pcl/registration/icp.h>
+//#include <pcl/registration/gicp.h> Broken in 1.4.0: http://www.pcl-users.org/GICP-in-PCL-1-4-td3635906.html
 
 using namespace std;
 
@@ -40,52 +46,76 @@ int getfiles (string dir, vector<string> &files, string const &extension)
 
 int main (int argc, char** argv)
 {
+	//Read file list
     string dir = argv[1];
     vector<string> files = vector<string>();
     getfiles(dir, files, ".ply");
-    std::cout << "Reading " << files[1] << endl;
+    std::cout << "Reading " << files[0] << endl;
 
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr model (new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr frame (new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PointCloud<pcl::PointXYZRGB> alligned_frame;
 	
-	// if (pcl::io::loadPLYFile<pcl::PointXYZRGBA> (dir + "/" + files[1], *cloud) == -1) //* load the file
-	// {
-	//     PCL_ERROR ("Couldn't read file \n");
-	//     return (-1);
-	// }
-	sensor_msgs::PointCloud2 cloud_blob;
-	pcl::io::loadPLYFile (dir + "/" + files[0], cloud_blob);
+	pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> outlier_filter;
+	outlier_filter.setMeanK (50);
+	outlier_filter.setStddevMulThresh (1.0);
+	
+	// Create the filtering object
+	pcl::VoxelGrid<pcl::PointXYZRGB> downsample_filter;
+	downsample_filter.setLeafSize (0.015f, 0.015f, 0.015f);
+	
+	//Initialize the model
+	if (pcl::io::loadPLYFile<pcl::PointXYZRGB>(dir + "/" + files[0], *model) == -1)
+	{
+	     PCL_ERROR ("Couldn't read first frame \n");
+	     return (-1);
+	}
+	outlier_filter.setInputCloud (model);
+	outlier_filter.filter (*model);
+	downsample_filter.setInputCloud (model);
+	downsample_filter.filter (*model);
+	pcl::io::savePLYFileBinary<pcl::PointXYZRGB>(files[0].substr(0,files[0].size()-4) + "_alligned.ply", *model);
+	
+	//Registration loop
+	for (size_t i=1; i<files.size(); i++) {
+		if (pcl::io::loadPLYFile<pcl::PointXYZRGB>(dir + "/" + files[i], *frame) == -1)
+		{
+			PCL_ERROR ("Couldn't read some frame \n");
+			return (-1);
+		}
+		outlier_filter.setInputCloud (frame);
+		outlier_filter.filter (*frame);
+		downsample_filter.setInputCloud (frame);
+		downsample_filter.filter (*frame);
 		
-	pcl::fromROSMsg (cloud_blob, *cloud);
-    
-    // pcl::PLYReader reader;
-    // reader.read(dir + "/" + files[1], *cloud);
+		//Set the ICP parameters
+		pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
+		//pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
+		//icp.setMaxCorrespondenceDistance(0.0001);
+		icp.setMaximumIterations(50);
+		icp.setRANSACOutlierRejectionThreshold(0.01); //default: 0.05m
+		//icp.setRANSACIterations(50);
+		icp.setTransformationEpsilon(0.01);
+		icp.setEuclideanFitnessEpsilon(0.01);
+		
+		//Allign the new frame
+		icp.setInputCloud(frame);
+		icp.setInputTarget(model);
+		std::cout << "Alligning " << files[i] << endl;
+		icp.align(alligned_frame);
+		
+		//Some debug printing
+		std::cout << "has converged:" << icp.hasConverged() << " score: " <<
+		icp.getFitnessScore() << std::endl;
+		std::cout << icp.getFinalTransformation() << std::endl;
+		
+		//Write the alligned frame
+		pcl::io::savePLYFileBinary<pcl::PointXYZRGB>(files[i].substr(0,files[i].size()-4) + "_alligned.ply", alligned_frame);
+		
+		*model = *frame;
+	}
 	    
-    std::cout << "Read " << cloud->width * cloud ->height << " points." << endl;
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-	  printf("%.3f %.3f %.3f %x\n", cloud->points[i].x, cloud->points[i].y, cloud->points[i].z, cloud->points[i].rgb);
-  // sensor_msgs::PointCloud2::Ptr cloud (new sensor_msgs::PointCloud2 ());
-  //   sensor_msgs::PointCloud2::Ptr cloud_filtered (new sensor_msgs::PointCloud2 ());
-  // 
-  //   // Fill in the cloud data
-  //   pcl::PCDReader reader;
-  //   // Replace the path below with the path where you saved your file
-  //   reader.read ("table_scene_lms400.pcd", *cloud); // Remember to download the file first!
-  // 
-  //   std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
-  //        << " data points (" << pcl::getFieldsList (*cloud) << ").";
-  // 
-  //   // Create the filtering object
-  //   pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
-  //   sor.setInputCloud (cloud);
-  //   sor.setLeafSize (0.01f, 0.01f, 0.01f);
-  //   sor.filter (*cloud_filtered);
-  // 
-  //   std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
-  //        << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
-  // 
-  //   pcl::PLYWriter writer;
-  //   writer.write ("downsampled.ply", *cloud_filtered, 
-  //          Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false, true);
+    
 
   return (0);
 }
